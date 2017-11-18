3e08f2ce63aa743876b04052358894ac087f55eb
jonathon reich <jonathonreich@MacBook-Air.local>
2017-11-18 10:36:10 -0500

    final hangman still confused

--- assets/javasript/game.js
+++ assets/javasript/game.js
@@ -1,6 +1,104 @@
-//record how many times a letter can be pressed 
+// //record how many times a letter can be pressed 
 
 
+// var doubleWord = ['a','b','c',
+// 				  'd','e','f',
+// 				  'g','h','i',
+// 				  'j','k','l',
+// 				  'm','n','o',
+// 				  'p','q','r',
+// 				  's','t','u',
+// 				  'v','w','x',
+// 				  'y','z'];
+
+
+
+// ///================================================================
+
+
+// //all the variables below that the computer can pick from//
+
+// var words = ['Cocaine', 'Money', 'Women', 'Tony', 'Montana','scar','power','boss','miami','drugs','Kingpin','Cuban'];
+
+
+// ///================================================================
+
+
+// //Tell the computer to pick a random word from the word bank created above
+// var word = words[Math.floor(Math.random() * words.length)];
+// var underscore = [' _ '];
+// var wrongletters = [];
+// var blanksandsucesses = [];
+// var rightguesscounter = 0;
+
+
+// //counters
+
+// var guessesLeft = 7
+// var rightguesscounter = 0
+
+// document.getElementById("guesses-left").innerHTML = guessesLeft;
+
+// console.log(word)
+
+// ///================================================================
+
+// //start game
+
+
+
+
+
+
+
+// //give the player amount of dashes and spaces in place of the word there guessing 
+
+// 			//var is all the words spilt into one letter 
+// 			var splitWordArray = word.split("");
+// 			console.log(splitWordArray);
+
+
+			
+// 		///////////
+		
+// 			for (var i = 0; i < splitWordArray.length; i++){
+
+
+// 				console.log(splitWordArray[i])
+
+
+			
+// 			}
+
+			
+				
+
+// 			//Populate blanks
+// 	for (let i = 0; i < word.length; i++) {
+// 					underscore.push(' _ ')
+				
+				
+//  var underscoreplace = document.getElementById("wordToGuess");
+	
+// 	var newunderscore = document.createElement("div")
+
+// 	newunderscore.innerHTML = underscore[i]
+
+// 	underscoreplace.appendChild(newunderscore)
+
+// 	newunderscore.setAttribute("style", "display:inline")
+// }
+
+
+
+// 	
+
+
+
+
+//GLOBAL VARIABLES
+//---------------------------------------
+// Used to record how many times a letter can be pressed
 var doubleWord = ['a','b','c',
 				  'd','e','f',
 				  'g','h','i',
@@ -10,98 +108,179 @@ var doubleWord = ['a','b','c',
 				  's','t','u',
 				  'v','w','x',
 				  'y','z'];
-
-
-
-///================================================================
-
-
-//all the variables below that the computer can pick from//
-
-var words = ['Cocaine', 'Money', 'Women', 'Tony', 'Montana','scar','power','boss','miami','drugs','Kingpin','Cuban'];
-
-
-///================================================================
-
-
-//Tell the computer to pick a random word from the word bank created above
-var word = words[Math.floor(Math.random() * words.length)];
-var underscore = ['_'];
-var wrongletters = [];
-
-
-//counters
-var wincount = 0;
-var losecount = 0;
-var guessesleft = 7
-var rightguesscounter = 0
-
-
-
-console.log(word)
-
-///================================================================
-
-//start game
-
-
-
-
-
-
-
-//give the player amount of dashes and spaces in place of the word there guessing 
-
-			//var is all the words spilt into one letter 
-			var splitWordArray = word.split("");
-			console.log(splitWordArray);
-
-
-			
-		///////////
-		
-			for (var i = 0; i < splitWordArray.length; i++){
-
-
-				console.log(splitWordArray[i])
-
-
-			
-			}
-
-			///make the underscores
-			// for (let i = 0; i < word.length; i++) {
-			// 		underscore.push('_')
-			// 	}
-				
-			
-			// 	console.log(underscore)
-
-			// document.getElementById("wordToGuess").appendChild(underscore)
-				
-				// var wordToGuessID = document.getElementById("wordToGuess");
-				
-				// for(var i = 0; i < underscore.length; i++){
-				// 	wordToGuessID.innerHTML(underscore[i]);
-
-				// }
-				
-
-			//Populate blanks
-	for (let i = 0; i < word.length; i++) {
-					underscore.push('_')
-				
-				
- var underscoreplace = document.getElementById("wordToGuess");
+//Holds the all the words
+var wordBank =['cocaine', 'money', 'women', 'tony', 'montana','scar','power','boss','miami','drugs','kingpin','Cuban'];
+//Holds choosenWord
+var choosenWord = "";
+//Holds letters in word
+var lettersInWord = [];
+//Holds number of blanks in word
+var numBlanks = 0;
+//Holds Blanks and successful guesses
+var blanksAndSuccesses =[];
+//Holds Wrong guesses
+var wrongLetters = [];
+//Counters
+var winCount = 0;
+var loseCount = 0;
+var guessesLeft = 9;
+var rightGuessCounter = 0;
+//FUNCTIONS
+//----------------------------------------
+function reset()
+{
+	//Chooses word randombly from the wordBank
+	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	//Splits the choosen word into individual letters
+	lettersInWord = choosenWord.split('');
+	//Get the number of blanks
+	numBlanks = lettersInWord.length;
 	
-	var newunderscore = document.createElement("div")
+	//RESET
+	//===========================================================
+	letterGuessed = 0;
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	doubleWord = ['a','b','c',
+					  'd','e','f',
+					  'g','h','i',
+					  'j','k','l',
+					  'm','n','o',
+					  'p','q','r',
+					  's','t','u',
+					  'v','w','x',
+					  'y','z'];
+	test=false;
+	startGame();
+}
+function startGame()
+{
+	//Chooses word randombly from the wordBank
+	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	//Splits the choosen word into individual letters
+	lettersInWord = choosenWord.split('');
+	//Get the number of blanks
+	numBlanks = lettersInWord.length;
+	
+	//RESET
+	//===========================================================
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	doubleWord = ['a','b','c',
+					  'd','e','f',
+					  'g','h','i',
+					  'j','k','l',
+					  'm','n','o',
+					  'p','q','r',
+					  's','t','u',
+					  'v','w','x',
+					  'y','z'];
 
-	newunderscore.innerHTML = underscore[i]
+	//Populate blanks
+	for(var i = 0; i< numBlanks; i++)
+	{
+		blanksAndSuccesses.push('_');
+		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
+	}
 
-	underscoreplace.appendChild(newunderscore)
+	//Changes HTML 
+	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
+	document.getElementById('numGuesses').innerHTML = guessesLeft;
+	document.getElementById('winCounter').innerHTML = winCount;
+	document.getElementById('lossCounter').innerHTML = loseCount;
+	document.getElementById('wrongGuesses').innerHTML = wrongLetters;
+	// Testing / Debugging
+	console.log(choosenWord);
+	console.log(lettersInWord);
+	console.log(numBlanks);
+	console.log(blanksAndSuccesses);
 }
 
+function compareLetters(userKey)
+{
+				console.log('WORKING!');
+				//If user key exist in choosen word then perform this function 
+				if(choosenWord.indexOf(userKey) > -1)
+				{
+					//Loops depending on the amount of blanks 
+					for(var i = 0; i < numBlanks; i++)
+					{
+						//Fills in right index with user key
+						if(lettersInWord[i] === userKey)
+						{
+							rightGuessCounter++;
+							blanksAndSuccesses[i] = userKey;
+							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
+						}	
+					}
+					//Test / Debug
+					console.log(blanksAndSuccesses);
+				}
+				//Wrong Keys
+				else
+				{
+					wrongLetters.push(userKey);
+					guessesLeft--;
+					//Changes HTML
+					document.getElementById('numGuesses').innerHTML = guessesLeft;
+					document.getElementById('wrongGuesses').innerHTML = wrongLetters;
+					//Test / Debug
+					console.log('Wrong Letters = ' + wrongLetters);
+					console.log('Guesses left are ' + guessesLeft);
+				}
+			
+			
+		
+}
+function winLose()
+{
+	// When number blanks if filled with right words then you win
+	if(rightGuessCounter === numBlanks)
+	{
+		//Counts Wins 
+		winCount++;
+		//Changes HTML
+		document.getElementById('winCounter').innerHTML = winCount;
+		alert('You Win');
+		reset();
+	}
+	// When number of Guesses reaches 0 then You lose
+	else if(guessesLeft === 0)
+	{
+		//Counts losses
+		loseCount++;
+		//Changes HTML
+		document.getElementById('lossCounter').innerHTML = loseCount;
+		alert('You Lose');
+		reset();
+	}
+}
 
+//MAIN PROCCESS
+//-------------------------------------------	
+//Initiates the Code
+startGame();
 
+document.onkeyup = function(event)
+{
+	test = true;
+	var letterGuessed = event.key;
+	for(var i = 0; i < doubleWord.length; i++)
+	{	
+		if(letterGuessed === doubleWord[i] && test === true)
+		{
+			var spliceDword = doubleWord.splice(i,1);
+			//Test / Debug
+			console.log('Double word is = ' + doubleWord[i])
+			console.log('Spliced Word is = ' + spliceDword);
 
-
+			compareLetters(letterGuessed);
+			winLose();
+		}
+	}		
+		
+}
